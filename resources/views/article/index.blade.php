72b1f44fdbc7b279968f5a3261ff6d7269d62dc1
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-10-26 12:50:20 +0300

    Add link to add a new article into the template

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -9,6 +9,7 @@
             {{ session('status') }}
         </div>
     @endif
+    <small><a href="{{ route('articles.create') }}">Добавить новую статью</a></small>
     @foreach($articles as $article)
         <h2><a href="{{ route('articles.show', ['id' => $article->id]) }}">{{$article->name}}</a></h2>
         <div>{{Str::limit($article->body, 200)}}</div>
